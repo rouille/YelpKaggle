21557d17d0bc83451f1103398476cf90fee9b411
rouille <b.rouille.d.orfeuil@gmail.com>
2018-02-09 14:39:53 -0800

    Write function path_to_tensor

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,9 @@
 import glob
 import re
+import tqdm
+import numpy
+
+from keras.preprocessing import image
 
 def path_to_images(img_dir):
     images = glob.glob(img_dir + '/' + '*.jpg')
@@ -7,3 +11,11 @@ def path_to_images(img_dir):
     tags = map(extract_tag, images)
 
     return images, tags
+
+def path_to_tensor(img_path):
+    # loads RGB image as PIL.Image.Image type
+    img = image.load_img(img_path, target_size = (224, 224) )
+    # convert PIL.Image.Image type to 3D tensor with shape (224, 224, 3)
+    x = image.img_to_array(img)
+    # convert 3D tensor to 4D tensor with shape (1, 224, 224, 3) and return 4D tensor
+    return numpy.expand_dims(x, axis = 0)
