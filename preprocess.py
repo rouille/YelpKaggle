0b18292623268a93a7f714d416884a1cf4d04b53
rouille <b.rouille.d.orfeuil@gmail.com>
2018-02-09 14:05:09 -0800

    style: Rename function get_files to path_to_images

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,9 @@
 import glob
 import re
 
-def get_files(path):
-    files = glob.glob(path + '/' + '*.jpg')
+def path_to_images(img_dir):
+    images = glob.glob(img_dir + '/' + '*.jpg')
     extract_tag = lambda x: int(re.match('.*/([0-9]+).jpg', x).group(1))
-    tags = map(extract_tag, files)
+    tags = map(extract_tag, images)
 
-    return files, tags
+    return images, tags
