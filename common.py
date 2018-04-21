66d30fa0c1418032fdb622319fcd40034253434a
rouille <b.rouille.d.orfeuil@gmail.com>
2018-04-20 17:48:22 -0700

    feat: Write function group_images

--- common.py
+++ common.py
@@ -1,5 +1,6 @@
 import tqdm
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 from keras.preprocessing import image
@@ -17,7 +18,7 @@ def decode_label(x):
 
 
 def path_to_tensor(img_path):
-    img = image.load_img(img_path, target_size=(224, 224) )
+    img = image.load_img(img_path, target_size=(224, 224))
     x = image.img_to_array(img)
     return np.expand_dims(x, axis=0)
 
@@ -33,6 +34,18 @@ def load_dataset(file):
     targets = data['target']
     return images, targets
 
+def group_images(img, file):
+    img2biz = pd.read_csv(file, header = 0, names = ['photo','business'])
+    img2biz = img2biz[img2biz['photo'].isin(img)]
+    biz2img = pd.DataFrame(img2biz.groupby('business')['photo'].apply(list))
+
+    biz = biz2img.index.values
+    img2idx = dict(zip(img,range(len(img))))
+    group = []
+    for b in biz:
+        photos = biz2img.get_value(b,'photo')
+        group.append([img2idx[p] for p in photos])
+    return group
 
 def history(model):
     plt.figure(figsize = (15, 5) )
